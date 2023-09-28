e88c2f7641bc5606c7e09f3e8d08a75d06d9418b
Конопляса <konoplyasa@gb-1.ru>
2023-09-28 22:57:25 +0300

    финал

--- main.py
+++ main.py
@@ -21,13 +21,21 @@ def Init():
                      default='kseniya')
     arg.add_argument('-rate', help='установить качество звука. По умолчанию 48000',
                      choices=[8000, 24000, 48000], default=48000)
-    arg.add_argument('-name', help='Имя сохраняемого файла', default=datetime.strftime(datetime.now(), '%H.%M.%S_%d.%m.%Y'))
+    arg.add_argument('-name', help='Имя сохраняемого файла',
+                     default=datetime.strftime(datetime.now(), '%H.%M.%S_%d.%m.%Y'))
+    arg.add_argument('-speed', help='Установка скорости озвчивания. По умолчанию medium',
+                     choices=['x-slow', 'slow', 'medium', 'fast', 'x-fast'],
+                     default='medium')
+    arg.add_argument('-pitch', help='Установка тона. По умолчанию medium',
+                     choices=['x-low', 'low', 'medium', 'high', 'x-high'],
+                     default='medium')
 
     args = arg.parse_args()
 
-    Torching(text_for_speach=args.text, voice=args.voice, rate=args.rate, file_name=f'{args.name}.wav')
+    Torching(text_for_speach=args.text, voice=args.voice, rate=args.rate,
+             pitch=args.pitch, speed=args.speed, file_name=f'{args.name}.wav')
 
-def Torching(*, text_for_speach, voice, rate, file_name):
+def Torching(*, text_for_speach, voice, rate, pitch, speed, file_name):
 
     device = torch.device('cpu')
     torch.set_num_threads(4)
@@ -43,9 +51,13 @@ def Torching(*, text_for_speach, voice, rate, file_name):
     model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
     model.to(device)
 
-    audio_paths = model.save_wav(text=text_for_speach,
+    audio_paths = model.save_wav(ssml_text=f'<speak><prosody pitch="{pitch}">'
+                                           f'<prosody rate="{speed}">'
+                                           f'{text_for_speach}'
+                                           f'</prosody></prosody></speak>',
                                  speaker=voice,
                                  sample_rate=rate,
                                  audio_path=f'output\\{file_name}')
 
 Init()
+
